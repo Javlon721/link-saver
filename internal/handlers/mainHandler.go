50034f75c7aa409e98c00d78c8547b9c49b0aa95
Javlon Talipov <javlon.talipov13@mail.ru>
2026-02-28 18:55:26 +0500

    clean up

--- internal/handlers/mainHandler.go
+++ internal/handlers/mainHandler.go
@@ -1,8 +1,6 @@
 package handlers
 
 import (
-	"fmt"
-
 	"github.com/Javlon721/link-saver/internal/templates"
 	tele "gopkg.in/telebot.v4"
 )
@@ -11,8 +9,6 @@ type MainHandler struct{}
 
 func (h MainHandler) HelpDeskHandler(ctx tele.Context) error {
 	message := templates.HelpDesk()
-
-	fmt.Println("Message:", message.Text)
 
 	return ctx.Send(message.Text, message.ParseMode)
 }
