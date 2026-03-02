5f355c42480173fc05be9373c4ffb765e9967325
Javlon Talipov <javlon.talipov13@mail.ru>
2026-03-02 13:48:26 +0500

    add new commands to HelpDesk

--- internal/templates/help.go
+++ internal/templates/help.go
@@ -10,9 +10,11 @@ import (
 func HelpDesk() types.Message {
 	return types.Message{
 		Text: fmt.Sprint(
-			"<b>What can this bot do:</b>\n\n",
-			"/help - prints help desk\n\n",
-			"/links - print all links that you send\n\n",
+			"<b>What can this bot do:</b>\n",
+			"/register - registers new user\n",
+			"/me - send current user info\n",
+			"/help - prints help desk\n",
+			"/links - print all links that you send\n",
 		),
 		ParseMode: tele.ModeHTML,
 	}
