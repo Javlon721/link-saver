069cdc979e1e8fac04e4aedeef5493a15e4e0735
Javlon Talipov <javlon.talipov13@mail.ru>
2026-03-03 17:37:03 +0500

    refactor: clean up HelpDesk info

--- internal/templates/help.go
+++ internal/templates/help.go
@@ -1,6 +1,7 @@
 package templates
 
 import (
+	"bytes"
 	"fmt"
 
 	"github.com/Javlon721/link-saver/internal/types"
@@ -8,18 +9,35 @@ import (
 )
 
 func HelpDesk() types.Message {
+	var buf bytes.Buffer
+
+	// essentials
+	fmt.Fprint(&buf, fmt.Sprint(
+		"<b>What can this bot do:</b>\n",
+		"/help - prints help desk\n",
+		"\n",
+	))
+
+	// all user story
+	fmt.Fprint(&buf, fmt.Sprint(
+		"<b>Your account info:</b>\n",
+		"/register - registers new user\n",
+		"/me - send current user info\n",
+		"/stop - deletes all your data (<b>DANGER</b>)\n",
+		"\n",
+	))
+
+	// all links story
+	fmt.Fprint(&buf, fmt.Sprint(
+		"<b>Your links info:</b>\n",
+		"/link - register new link\n",
+		"/links - print all links that you have\n",
+		"/linksBtns - print all links that you have + alter buttons\n",
+		"\n",
+	))
+
 	return types.Message{
-		Text: fmt.Sprint(
-			"<b>What can this bot do:</b>\n",
-			"/register - registers new user\n",
-			"/me - send current user info\n",
-			"/help - prints help desk\n",
-			"\n",
-			"/link - register new link\n",
-			"/links - print all links that you send\n",
-			"\n",
-			"/stop - deletes all your data (<b>DANGER</b>)\n",
-		),
+		Text:      buf.String(),
 		ParseMode: tele.ModeHTML,
 	}
 }
